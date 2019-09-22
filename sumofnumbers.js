c8edda60466ba8493dff4c1a41a710cd849e0e2f
KWong41 <46613890+KWong41@users.noreply.github.com>
2019-09-21 14:32:26 -1000

    updated eslint settings
    added eslint application to this project WOD and fixed the coding standards errors that ESLint suggested

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,15 +1,15 @@
-//E26: Five problems every software engineer should be able to solve (Part 1)
-//pWOD 1
-//First Attempt: 30 mins DNF
-//Second Attempt:  45 mins completed (used Dr. Johnson's lecture video)
-//Third Attempt: 11:52 (went back and forth of looking at the completed code
-//forgot to clone the repository after creating it
-//Fourth Attempt:
+// E26: Five problems every software engineer should be able to solve (Part 1)
+// pWOD 1
+// First Attempt: 30 mins DNF
+// Second Attempt:  45 mins completed (used Dr. Johnson's lecture video)
+// Third Attempt: 11:52 (went back and forth of looking at the completed code
+// forgot to clone the repository after creating it
+// Fourth Attempt:
 const testNums = [1, 2, 3, 4];
 
-function sumFor(nums){
+function sumFor(nums) {
   let total = 0;
-  for(const num of nums){
+  for (const num of nums) {
     total += num;
   }
   return total;
@@ -17,23 +17,28 @@ function sumFor(nums){
 
 console.log(sumFor(testNums));
 
-function sumWhile(nums){
+function sumWhile(nums) {
   let total = 0;
-  let i=0;
-  while(i<nums.length){
+  let i = 0;
+  while (i < nums.length) {
     total += nums[i];
     i++;
   }
   return total;
 }
 
-function sumRecursion(nums){
-  if(nums.length===0){
+console.log(sumWhile(testNums));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
     return 0;
-  }
-    return nums[0] + sumRecursion(nums.slice(1, nums.length));
+  } return nums[0] + sumRecursion(nums.slice(1, nums.length));
 }
 
-function sumTheSimpleWay(nums){
-  return _.reduce(nums, function (memo, num) { return memo + num; },0);
+console.log(sumRecursion(testNums));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
 }
+
+console.log(sumTheSimpleWay(testNums));
